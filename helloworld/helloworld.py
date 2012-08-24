115946bfa216fb549eb9b6f306f8f832d110a6fe
Pawel Szostek <pawel.szostek@gmail.com>
2012-08-25 00:41:16 +0200

    layout started

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,10 +1,22 @@
+from google.appengine.api import users
 import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.out.write('Hello, webapp2 World!')
+        user = users.get_current_user()
 
-application = webapp2.WSGIApplication([
+        if user:
+            self.response.headers['Content-Type'] = 'text/plain'
+            self.response.out.write('Hello, ' + user.nickname())
+        else:
+            self.redirect(users.create_login_url(self.request.uri))
+
+app = webapp2.WSGIApplication([
     ('/', MainPage)
 ], debug=True)
+
+def main():
+    application.run()
+
+if __name__ == "__main__":
+    main()
