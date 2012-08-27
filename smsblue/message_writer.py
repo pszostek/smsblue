9d108ea6bc784383c8fe03b20febb53297dacc97
Pawel Szostek <pawel.szostek@gmail.com>
2012-08-27 16:21:11 +0200

    weekend update

--- smsblue/message_writer.py
+++ smsblue/message_writer.py
@@ -4,72 +4,165 @@
 from parse_general import DayGeneral
 from parse_pictocast import DayDetailed
 
-class MessageWriter(object):
+class IMessageWriter(object):
     def __init__(self):
         pass
     
-    def write_general_message(self, days):
+    def write_message(self, days, *args):
         pass
     
-    def write_detailed_message(self, days):
+    def predict_message_length(self, days, *args):
         pass
 
-class PolishMessageWriter(MessageWriter):
-    def __init__(self):
-        MessageWriter.__init__(self)
         
-    def _general_message(self, day):
+class GeneralMessageWriter(IMessageWriter):
+    sample_day = None
+    def __init__(self):
+        import datetime
+        IMessageWriter.__init__(self)
+        if not self.__class__.sample_day:
+            self.__class__.sample_day =\
+            DayGeneral(name="wed", date=datetime.date(2012,8,2),\
+            place="Warsaw", t_high=18, t_low=5, wind=12, uv=4,\
+            sunshine=3, precip="<1")
+        
+        
+class DetailedMessageWriter(IMessageWriter):
+    sample_day = None
+    
+    def __init__(self):
+        import datetime
+        from parse_pictocast import HourEntry, DaytimeWeather
+        IMessageWriter.__init__(self)
+        _dw = DaytimeWeather(temp=13, temp_like=15, wind=5, wind_gusts=6, humid = 70, precip=5.0, precip_prob=50)
+        if not self.__class__.sample_day:
+            self.__class__.sample_day = \
+                DayDetailed(datetime.date(2012, 8, 4), 'Wednesday',\
+                place = 'Warsaw',\
+                hours = [HourEntry(2, _dw), HourEntry(5, _dw), HourEntry(8, _dw),\
+                HourEntry(11, _dw), HourEntry(14, _dw), HourEntry(17, _dw),\
+                HourEntry(20, _dw), HourEntry(23, _dw)])
+        
+        
+class PolishGeneralMessageWriter(GeneralMessageWriter):
+    def __init__(self):
+        GeneralMessageWriter.__init__(self)
+
+    def _day_message(self, day, features):
         assert isinstance(day, DayGeneral)
-        ret = ""
-        ret += "%d.%d: " % (day.date.month, day.date.day)
-        ret += "%d < %s < %d, " % (day.t_low, "temp", day.t_high)
-        ret += "wiatr %d km/h, " % day.wind
-        if day.uv:
-            ret += "uv-index %d, " % day.uv
-        ret += "%dh slonca, " % day.sunshine
-        ret += "%smm opadu." % day.precip
+        ret = []
+        if 'TEMP' in features:
+            ret.append("%d*C < %s < %d*C" % (day.t_low, "t", day.t_high))
+        if 'WIND' in features:
+            ret.append("%d km/h" % day.wind)
+        if 'UV' in features:
+            if day.uv:
+                ret.append("uv-index %d" % day.uv)
+        if 'SUNSHINE' in features:
+            ret.append("%dh" % day.sunshine)
+        if 'PRECIP' in features:
+            ret.append("%smm" % day.precip)
+
+        ret = ", ".join(ret)
+        if 'DATE' in features:
+            date = "%2.2d.%2.2d: " % (day.date.day, day.date.month)
+            ret = date + ret
         return ret
 
-    def _detailed_message(self, day):
+    def write_message(self, days, features):
+        assert isinstance(days, list)
+        assert len(days) > 0
+
+        days = [self._day_message(d, features) for d in days]
+        return " ".join(days)
+    
+    def predict_message_length(self, days, features):
+        return len(self.write_message([self.__class__.sample_day]*number_of_days, features))
+
+
+class PolishDetailedMessageWriter(DetailedMessageWriter):
+    def __init__(self):
+        DetailedMessageWriter.__init__(self)
+
+    def predict_message_length(self, number_of_days, features, hours):
+        return len(self.write_message([self.__class__.sample_day]*number_of_days, features, hours))
+
+    def _day_message(self, day, features, hours):
         assert isinstance(day, DayDetailed)
+        if not len(features):
+            raise RuntimeError("No features for detailed days specified")
 
-        ret = ""
-        for w in day.hours:
-            ret += "%s:00: %d*C %3.1fmm %d%% " % (w.hour, w.weather.temp, w.weather.precip, w.weather.precip_prob)
-        return ret
+        ret = []
+        hours = set(hours).intersection([hour for hour in day.hours])
+        hours = sorted(hours)
+        for hour in hours:
+            ret.append([])
+            ret[-1].append("%sh:" % hour)
+            if "TEMP" in features:
+                ret[-1].append("%d*C" % day.hours[hour].temp)
+            if "TEMP_LIKE" in features:
+                ret[-1].append("fl %d*C" % day.hours[hour].temp_like)
+            if "WIND" in features:
+                ret[-1].append("%dkm/h" % day.hours[hour].wind)
+            if "WIND_GUSTS" in features:
+                ret[-1].append("g%dkm/h" % day.hours[hour].wind_gusts)
+            if "HUMID" in features:
+                ret[-1].append("h%d%%" % day.hours[hour].humid)
+            if "PRECIP" in features:
+                ret[-1].append("%3.1fmm" % day.hours[hour].precip)
+            if "PRECIP_PROB" in features:
+                ret[-1].append("%d%%" % day.hours[hour].precip_prob)
 
-    def write_general_message(self, days):
+        hour_str = [" ".join(item) for item in ret]
+        return ". ".join(hour_str)
+
+    def write_message(self, days, features, hours):
         assert isinstance(days, list)
         assert len(days) > 0
 
-        place = days[0].place
-        days = [self._general_message(d) for d in days]
-        return "%s %s" % (place, " ".join(days))
+        days = [self._day_message(d, features, hours) for d in days]
+        return ' '.join(days)
 
-    def write_detailed_message(self, days):
-        assert isinstance(days, list)
-        assert len(days) > 0
+        
+class PolishMessageWriter(object):
+    def __init__(self):
+        self.detailed_writer = PolishDetailedMessageWriter()
+        self.general_writer = PolishGeneralMessageWriter()
 
-        place = days[0].place
-        days = [self._detailed_message(d) for d in days]
-        return "%s %s" % (place, " ".join(days))
-
-
-class EnglishMessageWriter(MessageWriter):
+    def write_message(self, detailed_days, general_days, number_detailed, number_general, detailed_args, general_args, hours):
+        message = detailed_days[0].place + " "
+        message += self.detailed_writer.write_message(detailed_days[0:number_detailed], detailed_args, hours)
+        message += '. '
+        message += self.general_writer.write_message(general_days[number_detailed:number_detailed+number_general], general_args)
+        return message
+        
+    def predict_message_length(self, detailed_days, general_days, \
+        number_detailed, number_general, detailed_features,\
+        general_features, hours):
+        return len(self.write_message(detailed_days, general_days,\
+            number_detailed, number_general, detailed_features,\
+            general_features, hours))
+        
+        
+class EnglishMessageWriter(IMessageWriter):
     pass
+
 
 if __name__ == "__main__":
     from parse_general import GeneralParser
     from parse_pictocast import PictocastParser
-    g = GeneralParser()
-    d = PictocastParser()
-    with open("../kielce_pl_19836.html") as f:
-        f = "".join(f.readlines())
-        lg = g.parse(f)
-    with open("../pictocast.html") as f:
-        f = "".join(f.readlines())
-        ld = d.parse(f)
-    mw = PolishMessageWriter()
-    msg = mw.write_detailed_message(ld[:1])
-    msg += mw.write_general_message(lg[1:3])
-    print(len(msg), msg)
+    general_parser = GeneralParser()
+    detailed_parser = PictocastParser()
+
+    with open("../kielce_pl_19836.html") as html_file:
+        html_file = "".join(html_file.readlines())
+        days_general = general_parser.parse(html_file)
+    with open("../pictocast.html") as html_file:
+        html_file = "".join(html_file.readlines())
+        days_detailed = detailed_parser.parse(html_file)
+
+    writer = PolishMessageWriter()
+    message = writer.write_message(days_detailed, days_general, 1, 1, ["TEMP", "PRECIP", "PRECIP_PROB"], ["TEMP", "DATE", "PRECIP"], [8,14,20] )
+    print writer.predict_message_length(days_detailed, days_general, 1, 1, ["TEMP", "PRECIP", "PRECIP_PROB"], ["TEMP", "DATE"], [8,14,20])
+    print len(message)
+    print(message)
--- smsblue/sms_sender.py
+++ smsblue/sms_sender.py
@@ -14,15 +14,30 @@ class SMSSender(object):
     
     def send(self, number, message, eco=True):
         c = httplib.HTTPConnection(self.host)
-        message = urllib.pathname2url(message)
-        if eco:
-            eco = 1
-        else:
-            eco = 0
-        c.request("GET", self.request % (self.account, self.password, number, eco, message))
-        r = c.getresponse()
-        return r
+        message_parts = self._divide_into_smss(message)
+        response_codes = []
+        for part in message_parts:
+            message = urllib.pathname2url(part)
+            if eco:
+                eco = 1
+            else:
+                eco = 0
+            c.request("GET", self.request % (self.account, self.password, number, eco, message))
+            response = c.getresponse()
+            response_codes.append(response)
+        return response_codes
 
+    def _divide_into_smss(self, message):
+        if len(message) <= 160:
+            return [message]
+        else:
+            ret = []
+            while len(message) > 160:
+                ret.append(message[:160])
+                message = message[160:]
+            ret.append(message)
+        return ret
+        
 if __name__ == "__main__":
     s = SMSSender("pawel.szostek@gmail.com", "30cca6d85c3a3e77169fcff6a9afcc58")
     r = s.send(number="886746648", message="dupa biskupa")
