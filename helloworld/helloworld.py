1712217d75588138dac863113c9736514a62c0e7
Pawel Szostek <pawel.szostek@gmail.com>
2012-09-07 18:23:43 +0200

    experiments

--- helloworld/helloworld.py
+++ helloworld/helloworld.py
@@ -1,18 +1,23 @@
 from google.appengine.api import users
+from google.appengine.ext.webapp import template
 import webapp2
+import os
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
+        path = os.path.join(os.path.dirname(__file__), 'new.html')
+        self.response.out.write(template.render(path, {}))
 
-        if user:
-            self.response.headers['Content-Type'] = 'text/plain'
-            self.response.out.write('Hello, ' + user.nickname())
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+class AddHandler(webapp2.RequestHandler):
+    def post(self):
+        self.response.out.write(self.request.get('intensity'))
+        self.response.out.write(self.request.get('days_detailed_length'))
+        self.response.out.write(self.request.get('days_detailed'))
+        self.response.out.write(self.request.get('days_general'))
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage)
+    ('/new', MainPage),
+    ('/add', AddHandler)
 ], debug=True)
 
 def main():
